c6a4a2983017af4afad8f53389f14721e533c793
PasquiFra <francesco.pasquinoni@gmail.com>
2024-02-21 19:59:05 +0100

    MILESTONE 1

--- index.php
+++ index.php
@@ -10,9 +10,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Password Generator</title>
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link rel="stylesheet" href="style\style.css">
 </head>
 <body>
-    
+    <header>
+        <h1>Strong Password Generator</h1>
+    </header>
+    <main>
+        <h3>Genera una password sicura</h3>
+
+    </main>
 </body>
 </html>
